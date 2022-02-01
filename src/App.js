87e5f91840dd7df671d80d4f7c207ddc7002322d
drielisonlopes <drielisonl@gmail.com>
2022-02-01 09:12:26 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./views/Home";
+import Produtos from "./views/Produtos";
+import Lojas from "./views/Lojas";
+import Contato from "./views/Contato";
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
+
+import "./App.css"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/Produtos" element={<Produtos/>} />
+        <Route path="/Lojas" element={<Lojas/>} />
+        <Route path="/Contato" element={<Contato/>} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
