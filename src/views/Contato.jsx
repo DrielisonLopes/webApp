f0b06bbf8dae0b1a383d420fa84b4fd57ce3d7b1
drielisonlopes <drielisonl@gmail.com>
2022-02-04 17:11:42 -0300

    finalizando Contato

--- src/views/Contato.jsx
+++ src/views/Contato.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Footer from "../components/Footer";
 
 import '../assets/css/Promocoes.css';
 import '../assets/css/Contato.css';
@@ -11,36 +10,35 @@ export default function Contato() {
         <div>
             <img class="backContato" src={backContato} />  
             <div class="contato">
-                <h2 class="">Contact Us</h2>
+                <h2 class="faleConosco">Fale conosco</h2>
 
                 <div class="formulario">
                     <form action="#" method="POST" class="" source="custom" name="form">
-                    <div class="inputsMenores">
-                        <div class="">
-                            <label for=""></label>
-                            <input type="text" placeholder="Digite seu nome" name="name" class="" required="" />
+                    <div class="inputsDivs">
+                        <div class="inputDiv">
+                            <label for="nome"></label>
+                            <input type="text" placeholder="Digite seu nome" name="nome" class="inputbox" required="" />
                         </div>
-                        <div class="">
-                            <label for="email-d70e" class=""></label>
-                            <input type="email" placeholder="Digite seu email" id="email-d70e" name="email" class="" required="" />
+                        <div class="inputDiv">
+                            <label for="email"></label>
+                            <input type="email" placeholder="Digite seu email" id="email" name="email" class="inputbox" required="" />
                         </div>
-                        <div class="">
-                            <label for="phone-d5b3" class="u-form-control-hidden u-label"></label>
-                            <input type="tel" placeholder="Número de telefone" id="phone-d5b3" name="phone" class="" required="" />
+                        <div class="inputDiv">
+                            <label for="phone"></label>
+                            <input type="tel" placeholder="Número de telefone" id="phone" name="phone" class="inputbox" required="" />
                         </div>
                     </div>
                         <div class="mensagem">
-                            <label for="message-d70e" class=""></label>
-                            <textarea placeholder="Digite sua mensagem" rows="6" cols="100" id="message-d70e" name="message" class="" required=""></textarea>
+                            <label for="mensagem" class=""></label>
+                            <textarea placeholder="Digite sua mensagem" id="mensagem" name="mensagem" class="" required=""></textarea>
                         </div>
-                        <div class="">
+                        <div>
                             <button class="btnEnviar">Enviar</button>
                         </div>
-                        <div class=""> Obrigado pela mensagem! Entraremos em contato assim que possível. </div>
+                        <div> Obrigado pela mensagem! Entraremos em contato assim que possível. </div>
                     </form>
                 </div>
             </div>
-            <Footer></Footer>
         </div>
     );
 }
